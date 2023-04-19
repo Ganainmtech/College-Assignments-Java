292514f0cee48ad5b9ce194297c6d16abeba51f4
SJKenny45 <sarajanedeveloper@gmail.com>
2023-04-19 22:19:36 +0100

    Update partTime.java

--- Assignment3/partTime.java
+++ Assignment3/partTime.java
@@ -3,32 +3,31 @@
 // --------------------------------------------------------
 
 package academicEmployees;
+
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
-public class partTime extends academicEmployees {
+public class partTime extends teacher {
 	public static Scanner sc = new Scanner (System.in);
-
-	public float examScriptsPay;
-
-	// Method to calculate monthly salary
+	public static DecimalFormat money = new DecimalFormat ("€0.00");
+	
+	public float hoursWorked;
+	
 	public void calcMonthlySalary() {
 		System.out.print("\nEnter the hours worked: ");
-		float hoursWorked = sc.nextFloat();
+		hoursWorked = sc.nextFloat();
 		
 		monthlySalary = hoursWorked * 34.03f;
 	}
 
-	// Method to calculate how much is earned from correcting exam scripts
-	public void calcCorrectedScripts() {
-		System.out.print("\nEnter the amount of scripts corrected: ");
-		int examScriptsCorrected = sc.nextInt();
-		
-		if (examScriptsCorrected > 0)
-			examScriptsPay = examScriptsCorrected * 10.18f;
-		}
-
-	// Method to calculate gross pay 
 	public void calcGrossPay() {
 		grossPay = monthlySalary + examScriptsPay;
 	}
+	
+	public void outPutWorkDetails() {
+		System.out.printf("\nCorrected Exam Scripts:%23s", + examScriptsCorrected + " | Total while €10.18 per script = " + money.format(examScriptsPay));
+		System.out.printf("\nPart Time Hours Worked:%23s", + hoursWorked + " | Total while €34.03 per hour = " + money.format(monthlySalary) + "\n");
+
+	}
 }
+
